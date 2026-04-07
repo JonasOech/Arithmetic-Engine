11c52936d4bdf3948b3febf076da7709ede38493
JonasÖchsner <jonasoechsner06@gmail.com>
2026-04-07 11:47:32 +0200

    Full parsing functionality

--- elements.py
+++ elements.py
@@ -4,17 +4,27 @@ from math import inf
 class Element:
     tag : str 
 
+    def __str__(self):
+        raise NotImplementedError
+
 class Constant(Element):
     value: int|float|complex|bool
     
     def __init__(self, value: int|float|complex|bool):
         self.value = value
 
+    def __str__(self):
+        return str(self.value)
+
 class Variable(Element):
     id : str
 
     def __init__(self, id: str):
         self.id = id
+
+    def __str__(self):
+        return str(self.id)
+
 
 class Expression(Element):
     type : str
@@ -36,4 +46,17 @@ class Expression(Element):
                 self.Numelements = 2
             case "("|")":
                 self.priority = 3
-                self.Numelements = int("inf")
+    
+    def __str__(self):
+        return str(self.type)
+
+class Function(Element):
+    type : str
+    NumElements: int
+
+    def __init__(self, type:str):
+        self.type = type
+    
+    def __str__(self):
+        return str(self.type)
+    
--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
-from treelib import Tree
-from elements import Constant, Variable, Expression
+from anytree import AnyNode
+from elements import Constant, Variable, Expression, Function
 from re import search as ReSearch
 
 class Parser:
-    def parse_from_txt(self, formula:str= "x1 * ( x2 + x3 )") -> Tree:
+    def parse_from_txt(self, formula:str= "x1 * ( x2 + x3 )"):
         """_summary_
         Accepts input of formulas in text form.
         Variables: x1, x2, x3, ...
@@ -13,34 +13,170 @@ class Parser:
             _type_: Tree Object
         
         """            
-        output = Tree()
+        # output = Node()
         stack = [] # All elements will be pushed to the stack. The tree will be built in the stack
 
-        form = formula.split(' ') # Make list of all expressions (Later replace with tokenizer)
-        form = [for x in form: classify_element(x))] # List now of element objects
+        form = formula.strip().split(' ') # Make list of all expressions (Later replace with tokenizer TODO)
 
-        output = _apply_expressions(form, 4)
+        form = [self._classify_element(x) for x in form] # List now of element objects
+        
+        # replace every const and var with a AnyNode
+        for i in range(len(form)): 
+            if type(form[i]) in [Constant, Variable]:
+                form[i] = AnyNode(name = str(form[i]), element = i)
 
+        # If everything succeded, it's a list with just a tree
+        output = self._apply_expressions(form) 
+        return output 
 
-    def _apply_expressions(self, formula:list[Constant|Variable|Expression], prio:int) -> Tree:
+    def _apply_expressions(self, formula:list[AnyNode|Expression|Function]) -> list[AnyNode]|AnyNode:
         """
-        Recursively applies expressions of a select prio and downwards
+        applies expressions recursively
         """
+        formulacopy = formula
 
-        if not max(x) for x in [element in formula if type(element) == Expression] == prio:
-            return _apply_expressions(formula, prio-1)
+        
+        # Apply functions , but just "F ( S )"
+        for i in range(len(formulacopy)):
+            # Perform type safety
+            if not (
+                i + 3 <= len(formula) and # avoid index errors
+                type(formula[i]) == Function and
+                type(formula[i+1]) == Expression and 
+                type(formula[i+2]) == AnyNode and
+                type(formula[i+3]) == Expression
+            ):
+                continue
+            # check if parenthesis match
+            if not (
+                str(formula[i+1]) == "(" and
+                str(formula[i+3]) == ")"
+            ):
+                continue
 
-        for element in formula:
-            if type(element) == Expression and element.priority == prio:
+            # We have a function!
+            formula[i] = AnyNode(name = str(formula[i]), children=[formula[i+2]], element = formula[i])
+            del formula[i+1:i+4]
+
+            return self._apply_expressions(formula)
+
+        # Apply Parentheses, but just "( S )"
+        for i in range(len(formulacopy)):
+            # Perform type safety
+            if not (
+                i + 2 < len(formula) and # avoid index errors
+                type(formula[i+0]) == Expression and 
+                type(formula[i+1]) == AnyNode and
+                type(formula[i+2]) == Expression
+            ):
+                continue
+            # check if parenthesis match
+            if not (
+                str(formula[i+0]) == "(" and
+                str(formula[i+2]) == ")"
+            ):
+                continue
+
+            # We have Parenthesis
+
+            formula[i] = formula[i+1]
+            del formula[i+1:i+3]
+
+            return self._apply_expressions(formula)
+
+
+        # Apply Exponentiation "S ^ S"
+        formula, r = self._apply_binary_operator(formula, "^", ltr=False)
+        if r: 
+            return self._apply_expressions(formula)
+
+        # Apply Multiplication "S S"
+        for i in range(len(formulacopy)):
+            if not ( 
+                    i+1 < len(formula) and
+                    # S S
+                    type(formula[i]) == AnyNode and 
+                    type(formula[i+1]) == AnyNode
+            ):
+                continue
+
+            formula[i] = AnyNode(name = "*", element = Expression("*"), children=[
+                formula[i],
+                formula[i+1]
+            ])
+            del formula[i+1]
+            return self._apply_expressions(formula)
+
+
+        # Apply Multiplication "S * S" and Division "S/s"
+        formula, r = self._apply_binary_operator(formula, ["*", "/"])
+        if r: 
+            return self._apply_expressions(formula)
+
+        # Apply Addition "S + S"
+        formula, r = self._apply_binary_operator(formula, ["+", "-"])
+        if r: 
+            return self._apply_expressions(formula)
+
+
+            
+        # If we arrive here, none of the above operations were made. Check if we are done.
+        if len(formula) == 1:
+            return formula[0]
+        else:
+            raise Exception("Didn't work")
+
+
+        
+    def _apply_binary_operator(self, formula:list[AnyNode|Expression|Function], op:str|list[str] = "+", ltr:bool = True) -> tuple[list[AnyNode|Expression|Function], bool]:
+        """ Will search the tree for binary operations like S * S, S + S, ...
+
+        Args:
+            formula (_type_): the formula
+            op (str, optional): the operator: "+", "/", ...
+            ltr (bool, optional): Left to right. Will search right to left if false
+
+        Returns:
+            list: the new function
+            bool: true if a change was performed
+        """
+        
+        indices = range(len(formula)) if ltr else range(len(formula) - 1, -1, -1)
+        for i in indices:
+            if not(
+                i+2 < len(formula) and
+                type(formula[i]) == AnyNode and
+                type(formula[i+1]) == Expression and
+                type(formula[i+2]) == AnyNode and
+                ( # op could be both list or str
+                    str(formula[i+1]) == op or
+                    str(formula[i+1]) in op
+                )
+            ):
+                continue
+
+
+            formula[i+1] = AnyNode(name = str(formula[i+1]), element = formula[i+1], children=[
+                formula[i],
+                formula[i+2]
+            ])
+            del formula[i+2]
+            del formula[i]
+
+            return formula, True
+
+        return formula, False
+
+         
+
 
     
-    def classify_element(self, element:str="x1") -> Constant|Variable|Expression:
-        if ReSearch("\d+", element):
-            return Constant(int(element))
-        elif ReSearch("x\d", element):
+    def _classify_element(self, element:str="x1") -> Constant|Variable|Expression|Function|AnyNode:
+        if ReSearch(r"^x\d", element):
             return Variable(element)
-        elif ReSearch("\*|\+|\-|\/|\(", element):
+        elif ReSearch(r"^\d+", element):
+            return Constant(int(element))
+        elif ReSearch(r"^[\*\+\-\/\(\)\^]$", element):
             return Expression(element)
-        
         else:
-            raise TypeError("Input can not be classified")
+            return Function(element)
